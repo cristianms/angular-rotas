aed98ad29b0cefb5fe3b029aa1ba30c30e6c1fc0
Cristian <cristian.ms@hotmail.com>
2019-07-26 10:33:41 -0300

    Adicionando padrão de url do html5 com #

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -47,7 +47,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes),
+        RouterModule.forRoot(appRoutes, {useHash: true}),
     ],
     exports: [
         RouterModule
